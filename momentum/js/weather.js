de22bcb3dc7ad7b0266a3a7a98cbb5a4fd91cc56
Denis Ivanchenko <sicknature24@yandex.com>
2023-02-16 02:44:00 +0300

    feat: add error handler for getWeather method

--- momentum/js/weather.js
+++ momentum/js/weather.js
@@ -4,6 +4,7 @@ const weatherDescription = document.querySelector('.weather__description');
 const weatherCityInput = document.querySelector('.weather__city-input');
 const weatherWind = document.querySelector('.weather__wind');
 const weatherHumidity = document.querySelector('.weather__humidity');
+const weatherError = document.querySelector('.weather__error');
 
 
 function setLocalStorage() {
@@ -27,14 +28,26 @@ function checkLocalStorage() {
 async function getWeather(city) {
     const url =
         `https://api.openweathermap.org/data/2.5/weather?q=${city}&lang=en&appid=d5d445865cf8362c1394aad7f296c972&units=metric`;
-    const res = await fetch(url);
-    const data = await res.json();
-    weatherIcon.className = 'weather__icon owf';
-    weatherIcon.classList.add(`owf-${data.weather[0].id}`);
-    weatherTemperature.textContent = `${data.main.temp.toFixed(0)}°C`;
-    weatherDescription.textContent = data.weather[0].description;
-    weatherWind.textContent = `Wind speed: ${data.wind.speed.toFixed(0)} m/s`;
-    weatherHumidity.textContent = `Humidity: ${data.main.humidity}%`
+    const response = await fetch(url);
+
+    if (response.ok) {
+        let json = await response.json();
+        weatherIcon.style.display = 'inline-block';
+        weatherIcon.className = 'weather__icon owf';
+        weatherIcon.classList.add(`owf-${json.weather[0].id}`);
+        weatherTemperature.textContent = `${json.main.temp.toFixed(0)}°C`;
+        weatherDescription.textContent = json.weather[0].description;
+        weatherWind.textContent = `Wind speed: ${json.wind.speed.toFixed(0)} m/s`;
+        weatherHumidity.textContent = `Humidity: ${json.main.humidity}%`
+        weatherError.textContent = '';
+    } else {
+        weatherError.textContent = `Error! city not found for '${weatherCityInput.value}'`;
+        weatherTemperature.textContent = '';
+        weatherDescription.textContent = '';
+        weatherWind.textContent = '';
+        weatherHumidity.textContent = '';
+        weatherIcon.style.display = 'none';
+    }
 }
 
 function run() {
